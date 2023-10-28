b5d32eac97a87902a051868838c9a723e08058a1
Mamoud Elawadi <mahmoudwagdi86@gmail.com>
2023-10-28 19:09:26 +0200

    refactor: extract graphql queries from home

---
+++ client/hooks/useTasks.ts
@@ -0,0 +1,45 @@
+import { useMutation, useQuery } from '@apollo/client';
+import { TaskInputType } from '@projectTypes/task';
+import { ADD_TASK, DELETE_TASK, GET_TASKS } from '@queries/tasks';
+
+export const useTasks = () => {
+  const {
+    data: { tasks },
+  } = useQuery(GET_TASKS);
+
+  const [addTaskMutation] = useMutation(ADD_TASK, {
+    update(cache, { data: { addTask } }) {
+      cache.writeQuery({
+        query: GET_TASKS,
+        data: {
+          tasks: addTask,
+        },
+      });
+    },
+  });
+
+  const [deleteTaskMutation] = useMutation(DELETE_TASK, {
+    update(cache, { data: { deleteTask } }) {
+      cache.writeQuery({
+        query: GET_TASKS,
+        data: {
+          tasks: deleteTask,
+        },
+      });
+    },
+  });
+
+  const addTask = async (task: TaskInputType) => {
+    await addTaskMutation({ variables: { task } });
+  };
+
+  const deleteTask = async (id: string) => {
+    await deleteTaskMutation({ variables: { id } });
+  };
+
+  return {
+    tasks,
+    addTask,
+    deleteTask,
+  };
+};
--- client/pages/Home/Home.tsx
+++ client/pages/Home/Home.tsx
@@ -1,43 +1,18 @@
-import { useMutation, useQuery } from '@apollo/client';
 import { TasksList } from '@client/components';
 import { Form } from '@client/components/Form/Form';
+import { useTasks } from '@client/hooks/useTasks';
 import { TaskInputType } from '@projectTypes/task';
-import { ADD_TASK, DELETE_TASK, GET_TASKS } from '@queries/tasks';
 import { Link } from 'react-router-dom';
 
 const Home = () => {
-  const {
-    data: { tasks },
-  } = useQuery(GET_TASKS);
-
-  const [addTask] = useMutation(ADD_TASK, {
-    update(cache, { data: { addTask } }) {
-      cache.writeQuery({
-        query: GET_TASKS,
-        data: {
-          tasks: addTask,
-        },
-      });
-    },
-  });
-
-  const [deleteTask] = useMutation(DELETE_TASK, {
-    update(cache, { data: { deleteTask } }) {
-      cache.writeQuery({
-        query: GET_TASKS,
-        data: {
-          tasks: deleteTask,
-        },
-      });
-    },
-  });
+  const { tasks, addTask, deleteTask } = useTasks();
 
   const onSubmitForm = async (task: TaskInputType) => {
-    await addTask({ variables: { task } });
+    await addTask(task);
   };
 
   const onDeleteClick = async (id: string) => {
-    await deleteTask({ variables: { id } });
+    await deleteTask(id);
   };
 
   return (
