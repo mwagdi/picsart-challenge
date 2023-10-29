292548159ed780f3063b525b610736b964c1a1df
Mamoud Elawadi <mahmoudwagdi86@gmail.com>
2023-10-29 21:00:05 +0100

    fix: fix responsiveness for user list

--- client/components/UserList/UserList.style.ts
+++ client/components/UserList/UserList.style.ts
@@ -4,6 +4,19 @@ export const List = styled.ul`
   padding: 0;
   list-style: none;
   display: grid;
-  grid-template-columns: repeat(3, 1fr);
+  grid-template-columns: repeat(1, 1fr);
+  grid-auto-rows: 1fr;
   gap: 16px;
+
+  @media (min-width: 768px) {
+    grid-template-columns: repeat(2, 1fr);
+  }
+
+  @media (min-width: 1024px) {
+    grid-template-columns: repeat(3, 1fr);
+  }
+
+  @media (min-width: 1280px) {
+    grid-template-columns: repeat(4, 1fr);
+  }
 `;
